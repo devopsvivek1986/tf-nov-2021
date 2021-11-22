371b1bb8e6a459dfe374834769f48653adf72a3d
vivekinfo1986 <vivekinfo1986@gmail.com>
2021-11-22 15:08:36 +0530

    Comming my Security Group-Vivek Kumar

--- main.tf
+++ main.tf
@@ -10,25 +10,41 @@ terraform {
 
 provider "aws" {
     region = "us-west-2"
-    access_key = ""
-    secret_key = ""
+    access_key = "AKIAZBO66PLYYPKDGAZD"
+    secret_key = "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"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_security_group" "vivek_allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  
+  ingress {
+    description      = "Allow HTTP to Instance"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
-    Name = "ExampleAppServerInstance - Nov 22 2021 - 111"
+    Name = "vivek_allow_http"
   }
 }
 
-
-resource "aws_instance" "app_server_2" {
+resource "aws_instance" "vivek_app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.vivek_allow_http.id]
 
   tags = {
-    Name = "ExampleAppServerInstance - Nov 22 2021 - 2"
+    Name = "vivek_app_server - Nov 22 2021"
   }
 }
+
